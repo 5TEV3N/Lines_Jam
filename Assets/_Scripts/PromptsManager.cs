7f8e8b78ea6957626f42c5ee775fc6a0201c6c9c
Steven Ramos <stevenramos0615@gmail.com>
2019-04-19 17:07:22 -0400

    Importing TextMesh Pro assets

---
+++ Assets/_Scripts/PromptsManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// This script will manager the prompts that appear in each round.
+
+public class PromptsManager : MonoBehaviour
+{
+    public Text UIPromptScreen;
+    public List<string> prompts = new List<string>();
+
+    private void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
